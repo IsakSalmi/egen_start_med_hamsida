0e9edb21a6e198bfeed0d98775d1ea73dceea11c
IsakSalmi <117453952+IsakSalmi@users.noreply.github.com>
2022-12-24 19:30:06 +0100

    Update Server.py

--- Server/Server.py
+++ Server/Server.py
@@ -7,4 +7,4 @@ def index():
     return render_template('Log_in.html')
 
 if __name__ == '__main__':
-    app.run(host='192.168.1.136', port='5000', debug=True)# mapping 
+    app.run(host='192.168.1.136', port='80', debug=True)# mapping 
